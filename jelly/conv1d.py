42e9dbb3ce73feb4fd8e85657185e60a922ec7d8
Divano <DDDivano@outlook.com>
2021-12-07 19:36:48 +0800

    add conv test case

---
+++ jelly/conv1d.py
@@ -0,0 +1,26 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# @author DDDivano
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+import paddle
+import torch
+import numpy as np
+from jelly import Jelly
+from jelly import randtool
+from jelly import get_dict
+
+
+def conv1d():
+    obj = Jelly(paddle_api=paddle.nn.functional.conv1d, torch_api=torch.nn.functional.conv1d)
+    data = randtool("float", 0, 1, [1, 1, 1])
+    paddle_param = get_dict(x=data, weight=np.ones(shape=[1, 1, 1]).astype("float64") * 0.3, bias=None, stride=1, padding=0,
+                            dilation=1, groups=1, data_format='NCL', name=None)
+    torch_param = get_dict(input=data, weight=np.ones(shape=[1, 1, 1]).astype("float64") * 0.3,
+                           bias=None, stride=1, padding=0, dilation=1, groups=1)
+    obj.set_paddle_param(paddle_param)
+    obj.set_torch_param(torch_param)
+    obj.run()
+
+
+if __name__ == '__main__':
+    conv1d()
---
+++ jelly/conv3d.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+# @author DDDivano
+# encoding=utf-8 vi:ts=4:sw=4:expandtab:ft=python
+import paddle
+import torch
+import numpy as np
+from jelly import Jelly
+from jelly import randtool
+from jelly import get_dict
+
+
+def conv3d():
+    data = randtool("float", 0, 1, [1, 1, 1, 1, 1])
+    obj = Jelly(paddle_api=paddle.nn.functional.conv3d, torch_api=torch.nn.functional.conv3d)
+    paddle_param = get_dict(x=data,
+                            weight=np.ones(shape=[1, 1, 1, 1, 1]).astype("float64") * 0.3,
+                            bias=np.zeros(shape=[1]).astype("float64"),
+                            stride=1,
+                            padding=0,
+                            )
+    torch_param = get_dict(x=data,
+                           weight=np.ones(shape=[1, 1, 1, 1, 1]).astype("float64") * 0.3,
+                           bias=np.zeros(shape=[1]).astype("float64"),
+                           stride=1,
+                           padding=0, )
+    obj.set_paddle_param(paddle_param)
+    obj.set_torch_param(torch_param)
+    obj.run()
+
+
+if __name__ == '__main__':
+    conv3d()
